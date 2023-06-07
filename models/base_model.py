c16275cbfc0f2601a5a6b62517c53e5fb240f8ce
Erica-Livia <e.ingabire1@alustudent>
2023-06-07 02:34:50 -0700

    updated base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -15,11 +15,18 @@ class BaseModel:
             self.updated_at = datetime.now()
             storage.new(self)
         else:
+            if "id" not in kwargs:
+                kwargs["id"] = str(uuid.uuid4())
+            if "updated_at" not in kwargs:
+                kwargs["updated_at"] = datetime.now().isoformat()
+            if "created_at" not in kwargs:
+                kwargs["created_at"] = datetime.now().isoformat()
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
             kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
+            if kwargs.get('__class__'):
+                del kwargs['__class__']
             self.__dict__.update(kwargs)
 
     def __str__(self):
