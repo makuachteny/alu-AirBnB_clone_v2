506942e203496e7774d63286a83fe0795b1b74f1
Erica-Livia <e.ingabire1@alustudent>
2023-06-07 02:27:56 -0700

    updated files

---
+++ tests/test_models/test_engine/test_db_storage.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import unittest
+from models.state import State
+from models import storage
+import os
+
+
+@unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') != 'db', "skip when the storage is not db")
+class TestDBStorage(unittest.TestCase):
+        """db storage tests"""
+
+        def test_state(self):
+            """ test state table"""
+            state = State(name="test")
+            state.save()
+            self.assertTrue("State.{}".format(state.id) in storage.all())
+            self.assertEqual(state.name, "test")
