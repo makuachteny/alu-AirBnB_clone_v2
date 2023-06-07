7e8f3e10f99d3b7f1c6bba652e0413272e0a0f92
Erica-Livia <e.ingabire1@alustudent>
2023-06-07 01:42:57 -0700

    updated files

---
+++ tests/test_console.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+
+import unittest
+
+
+from console import HBNBCommand
+from unittest.mock import patch
+from io import StringIO
+from models import storage
+import os
+
+stdout = StringIO()
+console = HBNBCommand()
+
+
+class ConsoleTestCase(unittest.TestCase):
+
+
+    def test_create(self):
+        if os.getenv("HBNB_TYPE_STORAGE")  != "db":
+            with patch('sys.stdout', stdout):
+                console.onecmd('create State name="california"')
+            self.assertEqual(len(stdout.getvalue()), 37)
