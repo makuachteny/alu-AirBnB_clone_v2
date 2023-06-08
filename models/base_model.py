58feca04c2c08c64981f2cac01978e1bf96da1e2
makuachteny <m.gatluak@alustudent.com>
2023-06-08 15:06:05 +0300

    imported sqlalchemy

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,8 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+from sqlalchemy import declarative_base
+from sqlalchemy import Column, String, DateTime, ForeignKey
 
 
 Base = declarative_base()
