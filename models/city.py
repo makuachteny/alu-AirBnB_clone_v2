5eaee6c47eb84a204536b43fe406660460e0de07
Erica-Livia <e.ingabire1@alustudent>
2023-06-08 05:01:51 -0700

    updated console.py

--- models/city.py
+++ models/city.py
@@ -7,6 +7,6 @@ from models.state import State
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
-    
-    name = Column(String(128), nullable = False)
-    state_id = Column(String(60), nullable = False, ForeignKey(state.id))
+
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), nullable=False, ForeignKey(state.id))
