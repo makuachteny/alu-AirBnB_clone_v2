09373619408c5127555357f98adc7aaefbd0f8f2
ericaing <109512001+Erica-Livia@users.noreply.github.com>
2023-06-08 18:54:48 +0200

    Update city.py

--- models/city.py
+++ models/city.py
@@ -2,7 +2,6 @@
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
-from models.state import State
 from sqlalchemy.orm import relationship
 
 class City(BaseModel, Base):
