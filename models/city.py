4a919938cd284676b73ba38b684dd138f3774823
ericaing <109512001+Erica-Livia@users.noreply.github.com>
2023-06-08 17:25:57 +0200

    Update city.py

--- models/city.py
+++ models/city.py
@@ -3,10 +3,11 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from models.state import State
+from sqlalchemy.orm import relationship
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
-
+state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    place = relationship("Place", backref='cities', cascade="delete")
     name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
