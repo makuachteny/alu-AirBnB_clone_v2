c685a18e3bb95eed7efe2665a0592d7fa6ae96ad
makuachteny <m.gatluak@alustudent.com>
2023-06-08 13:23:08 +0300

    Delete Object

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -9,10 +9,7 @@ class FileStorage:
     __objects = {}
 
     def all(self, cls=None):
-        """Returns a dictionary of models currently in storage
-        But if a cls is specified, returns a dictionary of objects
-        of that type.
-        """
+        """Returns a list of objects of a specific class from storage"""
         if cls is not None:
             new_obj_dict = {}
             for key, value in self.__objects.items():
@@ -45,6 +42,14 @@ class FileStorage:
         from models.amenity import Amenity
         from models.review import Review
 
+    def delete(self, obj=None):
+        """Deletes specified object from storage dictionary"""
+        if obj is None:
+            return
+        key = obj.__class__.__name__ + '.' + obj.id
+        if key in self.all():
+            del self.__objects[key]
+
         classes = {
             'BaseModel': BaseModel, 'User': User, 'Place': Place,
             'State': State, 'City': City, 'Amenity': Amenity,
@@ -58,10 +63,3 @@ class FileStorage:
                     self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
-
-    def delete(self, obj=None):
-        """Method that deletes obj from __objects"""
-        if obj is not None:
-            # get the key for this obj class name.id
-            key = obj.__class__.__name__ + '.' + obj.id
-            del self.__objects[key]
