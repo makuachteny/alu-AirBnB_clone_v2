990f37b1311270e7c68fe8b51068e6ef88980aa6
ericaing <109512001+Erica-Livia@users.noreply.github.com>
2023-06-08 19:01:38 +0200

    Update state.py

--- models/state.py
+++ models/state.py
@@ -14,7 +14,7 @@ storage = getenv("HBNB_TYPE_STORAGE")
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
-    name = column(String(128), nullable=False)
+    name = Column(String(128), nullable=False)
 
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         cities = relationship('City', backref='state',
