cf5db06d5a839c430e06d65ce1b872e4ddf88625
makuachteny <m.gatluak@alustudent.com>
2023-06-12 15:22:29 +0300

    Pycodestyle models

--- models/base_model.py
+++ models/base_model.py
@@ -30,9 +30,9 @@ class BaseModel:
             if "created_at" not in kwargs:
                 kwargs["created_at"] = datetime.now().isoformat()
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                    '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'], 
-                                                    '%Y-%m-%dT%H:%M:%S.%f')
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
             if kwargs.get('__class__'):
                 del kwargs['__class__']
             self.__dict__.update(kwargs)
