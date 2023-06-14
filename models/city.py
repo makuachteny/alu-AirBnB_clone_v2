ca4fc05543ee087af75ebc0bbae7ed5410544d66
makuachteny <m.gatluak@alustudent.com>
2023-06-14 15:45:29 +0300

    Debugging

--- models/city.py
+++ models/city.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel, Base
+from base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
