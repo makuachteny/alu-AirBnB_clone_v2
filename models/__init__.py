84f5ac95e8270a9b8684491611af42c0d6885c34
makuachteny <m.gatluak@alustudent.com>
2023-06-15 00:37:41 +0300

    updated

--- models/__init__.py
+++ models/__init__.py
@@ -5,7 +5,7 @@ import os
 
 storage = None
 if os.getenv("HBNB_TYPE_STORAGE") == "db":
-    from models.engine.db_storage import DBStorage
+    from models.engine.db_storage import DBStorage;
     storage = DBStorage()
     storage.reload()
 else:
