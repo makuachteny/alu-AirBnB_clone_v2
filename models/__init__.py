fa9458398a804d83781eab0dd8a32748d2af97aa
makuachteny <m.gatluak@alustudent.com>
2023-06-14 16:10:51 +0300

    Debugging

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStorage
+from engine.file_storage import FileStorage
 from models.engine.db_storage import DBStorage
 from os import getenv
 
