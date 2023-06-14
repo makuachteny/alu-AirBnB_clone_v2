847a2cf27aa02c2eb1cf7159a0aeb05b2bd11610
makuachteny <m.gatluak@alustudent.com>
2023-06-14 15:51:31 +0300

    Removed model module

--- models/__init__.py
+++ models/__init__.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStorage
+from engine.file_storage import FileStorage
 import os
 
 storage = None
 if os.getenv("HBNB_TYPE_STORAGE") == "db":
-    from models.engine.db_storage import DBStorage;
+    from models.engine.db_storage import DBStorage
     storage = DBStorage()
     storage.reload()
 else:
