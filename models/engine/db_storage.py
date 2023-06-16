d5b8ed4fc77f134a0d077842ca9025d96f016955
makuachteny <m.gatluak@alustudent.com>
2023-06-16 14:10:55 +0300

    update

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """Database storage engine"""
-import os
-from sqlalchemy import *
+import sqlalchemy
 from sqlalchemy.orm import sessionmaker, scoped_session
+import os
 from models.base_model import Base
 from models.city import City
 from models.state import State
