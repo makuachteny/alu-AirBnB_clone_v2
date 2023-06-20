16bb5664c65318e40f423eee9563cb6ae7644bc9
makuachteny <m.gatluak@alustudent.com>
2023-06-20 14:30:32 +0300

    compress before sending

---
+++ 1-pack_web_static.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# Fabric script that generates a .tgz archive from the web_static folder.
+
+def do_pack():
+    """Generates a .tgz archive from the web static folder"""
+    local("mkdir -p versions")
+
+    # Generate the archive name using the current timestamp
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+    archive_name = "web_static_{}.tgz".format(timestamp)
+
+    # Create the .tgz archive
+    result = local("tar-czvf versions/{} web_static".format(archive_name))
+
+    if result.succeeded:
+        return "versions/{}".format(archive_name)
+    else:
+        return None   
