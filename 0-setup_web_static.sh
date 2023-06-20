24a729a3e026471ca078c56e9bf01c2201a2f77d
makuachteny <m.gatluak@alustudent.com>
2023-06-20 13:48:32 +0300

    Prepare web-servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# Check if Nginx is already installed
 
+# Check if Nginx is already installed
 if ! command -v nginx &> /dev/null; then
     echo "Nginx is not installed. Installing now ..."
     # Update package lists
@@ -11,15 +11,15 @@ if ! command -v nginx &> /dev/null; then
     sudo service nginx start
     echo "Nginx installed and started successfully"
 else
-    echo "Nginx installed already"
+    echo "Nginx is already installed"
 fi
 
 # Create the /data/ folder if it doesn't exist
-if [! -d "/data"]; then
+if [ ! -d "/data" ]; then
     echo "/data/ folder does not exist. Creating now ..."
     sudo mkdir /data
-    sudo chown $USER:USER /data
-    echo "/data/ folder created successfully."
+    sudo chown -R ubuntu:ubuntu /data
+    echo "/data/ folder created and ownership set to ubuntu:ubuntu successfully."
 else
     echo "/data/ folder already exists"
 fi
@@ -28,8 +28,8 @@ fi
 if [ ! -d "/data/web_static" ]; then
     echo "/data/web_static/ folder does not exist. Creating now..."
     sudo mkdir /data/web_static
-    sudo chown $USER:$USER /data/web_static
-    echo "/data/web_static/ folder created successfully."
+    sudo chown -R ubuntu:ubuntu /data/web_static
+    echo "/data/web_static/ folder created and ownership set to ubuntu:ubuntu successfully."
 else
     echo "/data/web_static/ folder already exists."
 fi
@@ -38,8 +38,8 @@ fi
 if [ ! -d "/data/web_static/releases" ]; then
     echo "/data/web_static/releases/ folder does not exist. Creating now..."
     sudo mkdir /data/web_static/releases
-    sudo chown $USER:$USER /data/web_static/releases
-    echo "/data/web_static/releases/ folder created successfully."
+    sudo chown -R ubuntu:ubuntu /data/web_static/releases
+    echo "/data/web_static/releases/ folder created and ownership set to ubuntu:ubuntu successfully."
 else
     echo "/data/web_static/releases/ folder already exists."
 fi
@@ -48,8 +48,8 @@ fi
 if [ ! -d "/data/web_static/shared" ]; then
     echo "/data/web_static/shared/ folder does not exist. Creating now..."
     sudo mkdir /data/web_static/shared
-    sudo chown $USER:$USER /data/web_static/shared
-    echo "/data/web_static/shared/ folder created successfully."
+    sudo chown -R ubuntu:ubuntu /data/web_static/shared
+    echo "/data/web_static/shared/ folder created and ownership set to ubuntu:ubuntu successfully."
 else
     echo "/data/web_static/shared/ folder already exists."
 fi
@@ -58,8 +58,46 @@ fi
 if [ ! -d "/data/web_static/releases/test" ]; then
     echo "/data/web_static/releases/test/ folder does not exist. Creating now..."
     sudo mkdir /data/web_static/releases/test
-    sudo chown $USER:$USER /data/web_static/releases/test
-    echo "/data/web_static/releases/test/ folder created successfully."
+    sudo chown -R ubuntu:ubuntu /data/web_static/releases/test
+    echo "/data/web_static/releases/test/ folder created and ownership set to ubuntu:ubuntu successfully."
 else
     echo "/data/web_static/releases/test/ folder already exists."
 fi
+
+# Create a fake HTML file /data/web_static/releases/test/index.html
+echo "Creating fake HTML file /data/web_static/releases/test/index.html ..."
+echo "<html><body>Holberton School</body></html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+sudo chown -R ubuntu:ubuntu /data/web_static/releases/test/index.html
+echo "Fake HTML file created successfully."
+
+# Create or recreate the symbolic link /data/web_static/current
+if [ -L "/data/web_static/current" ]; then
+    echo "Symbolic link /data/web_static/current already exists. Deleting..."
+    sudo rm "/data/web_static/current"
+fi
+
+echo "Creating symbolic link /data/web_static/current ..."
+sudo ln -s "/data/web_static/releases/test" "/data/web_static/current"
+echo "Symbolic link created successfully."
+# Nginx configuration file
+echo "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root /var/www/html;
+    index index.html index.htm index.nginx-debian.html;
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+    location /redirect_me {
+        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+    }
+    error_page 404 /404.html;
+    location = /404.html {
+        root /var/www/html;
+        internal;
+    }
+}" > /etc/nginx/sites-enabled/default
+# Restart Nginx
+sudo service nginx start
