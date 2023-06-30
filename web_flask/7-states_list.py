336430fbb0e51fe3bfab4697d5b6554a910cb42b
makuachteny <m.gatluak@alustudent.com>
2023-06-30 09:39:22 +0300

    Executable

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" A flask script that returns state object's id and name
+"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """ display a HTML page """
+    # return a list of State object.
+    states = list(storage.all(State).values())
+    states = sorted(states, key=lambda state: state.name)
+    return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    """ remove the current SQLAlchemy Session """
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
