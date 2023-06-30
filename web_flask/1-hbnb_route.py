f30a99eb706712c6d26aca43a813aa38a5e96301
makuachteny <m.gatluak@alustudent.com>
2023-06-30 10:07:10 +0300

    Changing route

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -6,7 +6,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
+@app.route("/hbnb", strict_slashes=False)
 def hello():
     """ Hello HBNB! """
     return "Hello HBNB!"
