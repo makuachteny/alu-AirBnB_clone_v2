a1805586af766283ecda02a1e916c88e9a36b144
makuachteny <m.gatluak@alustudent.com>
2023-06-30 10:03:22 +0300

    Chnaging the route

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,10 +6,11 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
+@app.route("/hbnb", strict_slashes=False)
 def hello():
     """ Hello HBNB! """
     return "Hello HBNB!"
+
 
 
 if __name__ == '__main__':
